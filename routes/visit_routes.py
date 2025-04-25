c836d72515d6ef84ec95782640eb26816958fcf8
YAAGNAVALKYA <yaagnavalkyashankar166@gmail.com>
2025-04-25 15:52:00 +0530

    25/04 commit

--- routes/visit_routes.py
+++ routes/visit_routes.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 from services.visit_services import VisitServices
-from models import log_model,visit_model,vitals_model
+from models.log_model import LogModel
+from models.visit_model import VisitModel
+from models.vitals_model import VitalsModel
 router=APIRouter()
 service=VisitServices()
 
 @router.post("/add-visit")
-async def add_visit(visit_data:visit_model.VisitModel):
+async def add_visit(visit_data:VisitModel):
     return await service.add_visit(visit_data)
 
 @router.post("/add-visit-details.{caregiver_id}")
@@ -13,9 +15,9 @@ async def add_visit_details():
     return
 
 @router.post("/add-vitals/{patient_id}")
-async def add_vitals(vitals_data:vitals_model.VitalsModel):
+async def add_vitals(vitals_data:VitalsModel):
     return await service.add_vitals(vitals_data)
 
 @router.post("/finish-visit/{visit_id}")
-async def finish_visit(log_data:log_model.LogModel):
+async def finish_visit(log_data:LogModel):
     return await service.finish_visit(log_data)
