ae650818050c98c47d6bb53d84475c795cfcadc1
YAAGNAVALKYA <yaagnavalkyashankar166@gmail.com>
2025-04-28 15:38:46 +0530

    23/04 commit

--- models/visit_model.py
+++ models/visit_model.py
@@ -1,16 +1,18 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, Field
 from datetime import time
 from typing import Optional
 from enum import Enum
-class StatusEnum(str,Enum):
-    ACTIVE="DONE"
-    INACTIVE="PENDING"
+
+class StatusEnum(str, Enum):
+    ACTIVE = "DONE"
+    INACTIVE = "PENDING"
+
 class VisitModel(BaseModel):
-    patient_id:str=Field(...)
-    caregiver_id:str=Field(...)
-    scheduled_time:time=Field(...)
-    visit_status:StatusEnum.INACTIVE
-    location:list[str]=Field(...)
-    start_time:Optional[time]=Field(...)
-    end_time:Optional[time]=Field(...)
-    remarks:str=Field(...)
+    patient_id: str = Field(...)
+    caregiver_id: str = Field(...)
+    scheduled_time: time = Field(...)
+    visit_status: StatusEnum = Field(...)
+    location: list[str] = Field(...)
+    start_time: Optional[time] = Field(...)
+    end_time: Optional[time] = Field(...)
+    remarks: str = Field(...)
