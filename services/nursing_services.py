b9073794b186cd8e31a9a023cd9086a0ce6cee8b
YAAGNAVALKYA <yaagnavalkyashankar166@gmail.com>
2025-05-07 18:20:16 +0530

    07/05 commit

--- services/nursing_services.py
+++ services/nursing_services.py
@@ -13,10 +13,20 @@ class NursingServices:
             raise HTTPException(status_code=500, detail="Adding nursing service failed!")
     @staticmethod
     async def get_nursing_tasks(id):
-        task_details=await nursing.find_one({"id": id})
-        if not task_details:
+        cursor= nursing.find({"id": id})
+        tasks = []
+        async for task in cursor:
+            task["_id"]=str(task["_id"])
+            task.pop("_id",None)
+            task.pop("id",None)
+            tasks.append(task)
+        if not tasks:   
             raise HTTPException(status_code=404, detail=f"Nursing task with ID {id} not found!")
-        task_details["_id"]=str(task_details["_id"])
-        task_details.pop("_id",None)
-        return task_details
+        return tasks
+        # task_details=await nursing.find_one({"id": id})
+        # if not task_details:
+        #     raise HTTPException(status_code=404, detail=f"Nursing task with ID {id} not found!")
+        # task_details["_id"]=str(task_details["_id"])
+        # task_details.pop("_id",None)
+        # return task_details
     
